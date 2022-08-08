7076492f67e124ae123df6722886dff0a1cb80d5
Kyle Johnsen <kjohnsen@uga.edu>
2022-08-08 15:50:35 -0400

    add a test file to the data folder and make program output the number of files in data as well as the time

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 # This program is an example
 import time
+import os
+num_files = len(os.listdir("data"))
 file = open("res/time.txt","w")
-file.write(f"The program was last run at {time.ctime()}")
+file.write(f"The program was last run at {time.ctime()}\n")
+file.write(f"There are {num_files} in the data folder")
+
